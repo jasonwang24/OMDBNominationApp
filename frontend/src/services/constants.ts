89ff0bd82b88534f7e6556417b40ca610ccb5310
Jason <j.wang2216@gmail.com>
2021-01-07 01:38:05 -0500

    changed endpoint

--- frontend/src/services/constants.ts
+++ frontend/src/services/constants.ts
@@ -1,10 +1,10 @@
 export const ENDPOINTS = {
   OMDBSEARCH: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`,
   OMDBSEARCHMOVIEINFO: `http://www.omdbapi.com/?plot=short&apikey=${process.env.REACT_APP_API_KEY}`,
-  NOMINATIONSUBMISSION: "http://localhost:5000/movies/add",
-  USERSTORE: "http://localhost:5000/users/add",
-  CHECKUSER: "http://localhost:5000/users/checkuser",
-  RANKINGS: "http://localhost:5000/movies/top",
+  NOMINATIONSUBMISSION: "https://omdb-nomination.herokuapp.com/movies/add",
+  USERSTORE: "https://omdb-nomination.herokuapp.com/users/add",
+  CHECKUSER: "https://omdb-nomination.herokuapp.com/users/checkuser",
+  RANKINGS: "https://omdb-nomination.herokuapp.com/movies/top",
 };
 
 export const SWRKeys = {
